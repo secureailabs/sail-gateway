9906eea47c3a5eb3584fcc2df3577aefc4cf543b
Prawal Gangwar <prawal.gangwar@gmail.com>
2023-05-12 12:11:54 +0530

    nginx uses dns server

--- dns-server/dns_resolution.py
+++ dns-server/dns_resolution.py
@@ -1,11 +1,12 @@
 import redis
-from dnslib import QTYPE, RCODE, RR, A, DNSError, DNSRecord
+from dnslib import AAAA, QTYPE, RCODE, RR, A, DNSError, DNSRecord
 from dnslib.server import BaseResolver, DNSServer
 
 # Hardcoded DNS resolutions in a dictionary
 dns_records = {
     "example.com.": "172.20.100.11",
-    "test.com.": "192.0.2.2"
+    "test.com.": "192.0.2.2",
+    "dnscrud.": "127.0.0.1"
 }
 
 redis_instance = redis.Redis(host='localhost', port=6379, decode_responses=True)
@@ -15,10 +16,13 @@ class HardcodedResolver(BaseResolver):
         reply = request.reply()
         qname = request.q.qname
 
+        # Add a q type
+        qtype = request.q.qtype
+
         # Check if the domain is in the database
         domain = redis_instance.get(str(qname))
-        if domain:
-            reply.add_answer(RR(rname=qname, rtype=QTYPE.A, rdata=A(domain), ttl=60))
+        if domain and qtype == QTYPE.A:
+            reply.add_answer(RR(rname=qname, rtype=qtype, rdata=A(domain), ttl=60))
         else:
             reply.header.rcode = RCODE.NXDOMAIN
 
